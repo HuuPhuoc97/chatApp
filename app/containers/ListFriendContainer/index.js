afb6b638a84f54955ea4993e4851798cb7994689
HuuPhuoc97 <laptrinhhuuphuoc@gmail.com.vn>
2019-11-25 11:34:47 +0700

    edit login

--- app/containers/ListFriendContainer/index.js
+++ app/containers/ListFriendContainer/index.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import ItemFanPage from '../../components/ItemFanPage';
+import ListFriend from '../../components/ListFriends';
 // import * as actions from './actions';
 import './index.css';
-// import logo from './../../images/123.jpg';
-function ListFriend(props) {
+
+function ListFriendContainer(props) {
   const [keyword, setKeyword] = useState('');
   let list = props.data.listFriend;
   console.log('list ', list);
@@ -19,26 +21,58 @@ function ListFriend(props) {
     const { target } = event;
     setKeyword(target.value);
   };
-  const elementPage = list.map((page, index) => {
-    console.log(page);
+  const elementPage = list.map((item, index) => {
     return (
-      <div className="col-md-4 p-2">
-        <div className="card ">
-          <div className="card-body">
-            <div className="d-flex ">
-              <img src={page.picture.data.url} className="logo" alt="logo" />
-              <h5>{page.name}</h5>
-            </div>
-          </div>
-          <div className="card-footer">
-            <button type="button" className="btn btn-info  float-right">
-              Truy cập
-            </button>
-          </div>
-        </div>
-      </div>
+      <ItemFanPage
+        key={index}
+        urlImage={item.picture.data.url}
+        userName={item.name}
+      />
     );
   });
+
+  useEffect(() => {
+    (function(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+
+    window.fbAsyncInit = () => {
+      FB.init({
+        appId: '413811969495076', //Change with your Facebook app id
+        autoLogAppEvents: true,
+        cookie: true,
+        xfbml: true,
+        version: 'v5.0',
+      });
+
+      FB.Event.subscribe('auth.statusChange', response => {
+        if (response.authResponse) {
+          checkLoginState();
+        } else {
+          console.log(
+            '[FacebookLoginButton] User cancelled login or did not fully authorize.',
+          );
+        }
+      });
+    };
+  });
+
+  const logout = () => {
+    FB.logout(function(response) {
+      console.log('response logout', response.authResponse.status);
+
+      console.log('trang thai logout', response);
+      // props.onSetAccount(null);
+    });
+  };
   return (
     <div id="page">
       <div className="col-xl-12  ml-auto py-2 header-page ">
@@ -60,7 +94,7 @@ function ListFriend(props) {
           <div className="col-md-3 button-logout ">
             <img src={props.data.image} className="logo" alt="logo" />
             <span className="user-name">{props.data.name}</span>
-            <button type="button" className="btn btn-primary">
+            <button type="button" className="btn btn-primary" onClick={logout}>
               Logout
             </button>
           </div>
@@ -70,14 +104,19 @@ function ListFriend(props) {
         <h5 className="p-2">CHỌN PAGE BẮT ĐẦU : </h5>
         <div className="row m-3">{elementPage}</div>
       </div>
+
     </div>
+    // <ListFriend
+    // />
   );
 }
 const mapStateToProps = (state, props) => ({
   data: state.login.account,
 });
-const mapDispatchToProps = (dispatch, props) => ({});
+const mapDispatchToProps = (dispatch, props) => ({
+  // onSetAccount: value => dispatch(actions.setAccount(value)),
+});
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-)(ListFriend);
+)(ListFriendContainer);
